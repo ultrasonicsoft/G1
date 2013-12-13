2286d9e56a95542457a45a2b1e8517e3279889cb
ulrasonicsoft <balramchavan@gmail.com>
2013-12-13 23:09:49 +0530

    new files added

---
+++ GlassProductManager/Forms/Commander Mode/CustomerMasterContent.xaml.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ultrasonicsoft.Products;
+
+namespace GlassProductManager
+{
+    /// <summary>
+    /// Interaction logic for SettingsContent.xaml
+    /// </summary>
+    public partial class CustomerMasterContent : UserControl
+    {
+        private ListCollectionView _CustomerListForSearch;
+
+        public CustomerMasterContent()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (_CustomerListForSearch == null || txtSearch.Text == "Search")
+                    return;
+                if (txtSearch.Text != "")
+                {
+                    if (_CustomerListForSearch.CanFilter)
+                    {
+                        _CustomerListForSearch.Filter =
+                                new Predicate<object>(ContainsIt);
+
+                        FilterIt();
+                    }
+                    else
+                    {
+                        _CustomerListForSearch.Filter = null;
+                    }
+                }
+                else
+                {
+                    _CustomerListForSearch.Filter = null;
+                    FilterIt();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+        }
+
+        private void txtSearch_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtSearch.Text == "Search")
+            {
+                txtSearch.Text = string.Empty;
+            }
+        }
+
+        private void txtSearch_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtSearch.Text == string.Empty)
+            {
+                txtSearch.Text = "Search";
+            }
+        }
+
+        public bool ContainsIt(object value)
+        {
+            try
+            {
+                CustomerDetails currentRow = value as CustomerDetails;
+
+                if (dgCustomerList.Columns.Count > 1)
+                {
+                    if (currentRow != null)
+                    {
+                        if (IsSearchCriteriaMatched(currentRow)) return true;
+                    }
+                }
+                else
+                {
+                    if (IsSearchCriteriaMatched(currentRow)) return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+            return false;
+        }
+
+        private bool IsSearchCriteriaMatched(CustomerDetails currentRow)
+        {
+            return currentRow.FirstName.ToString()
+                                              .ToLower()
+                                              .Contains(txtSearch.Text
+                                                                .ToLower()) ||
+                                        currentRow.LastName.ToString()
+                                              .ToLower()
+                                              .Contains(txtSearch.Text
+                                                                .ToLower()) ||
+                                    currentRow.Address.ToString()
+                                              .ToLower()
+                                              .Contains(txtSearch.Text
+                                                                .ToLower()) ||
+
+                                    currentRow.Phone.ToString()
+                                              .ToLower()
+                                              .Contains(txtSearch.Text
+                                                                .ToLower()) ||
+                                    currentRow.Fax.ToString()
+                                              .ToLower()
+                                              .Contains(txtSearch.Text
+                                                                .ToLower()) ||
+                                    currentRow.Email.ToString()
+                                              .ToLower()
+                                              .Contains(txtSearch.Text
+                                                                .ToLower()) ||
+                                    currentRow.Misc.ToString()
+                                              .ToLower()
+                                              .Contains(txtSearch.Text
+                                                                .ToLower());
+        }
+
+        public void FilterIt()
+        {
+            try
+            {
+                dgCustomerList.ItemsSource = null;
+                ObservableCollection<CustomerDetails> fileList = new ObservableCollection<CustomerDetails>();
+
+                foreach (CustomerDetails row in _CustomerListForSearch)
+                {
+                    fileList.Add(row);
+                }
+                dgCustomerList.ItemsSource = fileList;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FillCustomerDetails();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+        }
+
+        private void FillCustomerDetails()
+        {
+            var result = BusinessLogic.GetAllCustomerDetails();
+            //dgCustomerList.ItemsSource = result;
+            _CustomerListForSearch = new ListCollectionView(result);
+
+            ListCollectionView collection = new ListCollectionView(result);
+            collection.GroupDescriptions.Add(new PropertyGroupDescription("FullName"));
+            dgCustomerList.ItemsSource = collection;
+        }
+
+        private void dgCustomerList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //OpenWorksheet();
+        }
+
+        private void OpenWorksheet()
+        {
+            Dashboard parent = Window.GetWindow(this) as Dashboard;
+
+            WorksheetEntity entity = dgCustomerList.SelectedItem as WorksheetEntity;
+
+            if (entity == null)
+            {
+                return;
+            }
+            if (parent != null)
+            {
+                DashboardMenu sideMenu = parent.ucDashboardMenu.CurrentPage as DashboardMenu;
+                DashboardHelper.ChangeDashboardSelection(parent, sideMenu.btnWorksheet);
+                WorksheetContent newQuote = new WorksheetContent(true, entity.QuoteNumber);
+                parent.ucMainContent.ShowPage(newQuote);
+            }
+        }
+
+        private void btnOpenWorksheet_Click(object sender, RoutedEventArgs e)
+        {
+            OpenWorksheet();
+        }
+
+        private void btnDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteCustomer();
+        }
+
+        private void DeleteCustomer()
+        {
+            if (FirmSettings.IsAdmin == false)
+            {
+                Helper.ShowErrorMessageBox("Only Administrator can delete customer");
+                return;
+            }
+            CustomerSmartDataEntity selectedCustomer = dgCustomerList.SelectedItem as CustomerSmartDataEntity;
+            if (selectedCustomer == null)
+            {
+                Helper.ShowErrorMessageBox("Please select customer to delete.");
+                return;
+            }
+
+            var result = Helper.ShowQuestionMessageBox("All information of customer will be deleted. Are you sure to delete?");
+            if (result == MessageBoxResult.Yes)
+            {
+                BusinessLogic.DeleteCustomer(selectedCustomer.ID);
+                Helper.ShowInformationMessageBox("Customer deleted successfully!");
+                FillCustomerDetails();
+                dgCustomerList.CanUserAddRows = false;
+            }
+        }
+
+    }
+}
