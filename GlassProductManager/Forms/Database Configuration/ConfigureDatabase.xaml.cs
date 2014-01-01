ccd85e618a289b4c3a657ec3adad84f18ec0e1d4
ulrasonicsoft <balramchavan@gmail.com>
2014-01-01 15:04:00 +0530

    configureDB

---
+++ GlassProductManager/Forms/Database Configuration/ConfigureDatabase.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DBHelper;
+
+namespace GlassProductManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ConfigureDatabase : Window
+    {
+        public bool IsDatabaseReady { get; set; }
+
+        public ConfigureDatabase()
+        {
+            InitializeComponent();
+
+            DisplayCurrentConnectionString();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtServerName.Text) || string.IsNullOrEmpty(txtDatabaseName.Text) || string.IsNullOrEmpty(txtUserName.Text) || string.IsNullOrEmpty(txtPassword.Password))
+                {
+                    Helper.ShowErrorMessageBox("Please provide required details!");
+                    return;
+                }
+
+                Properties.Settings.Default.ConnectionString = string.Format(System.Configuration.ConfigurationSettings.AppSettings["defaultConStr"], 
+                                txtServerName.Text, txtDatabaseName.Text, txtUserName.Text, txtPassword.Password);
+                SQLHelper.ConnectionString = Properties.Settings.Default.ConnectionString;
+                Properties.Settings.Default.Save();
+
+                Helper.ShowInformationMessageBox("Database server configured successfully!");
+                IsDatabaseReady = true;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                Helper.ShowErrorMessageBox(ex.Message);
+                IsDatabaseReady = false;
+            }
+        }
+
+        private void DisplayCurrentConnectionString()
+        {
+            if (false == string.IsNullOrEmpty(Properties.Settings.Default.ConnectionString))
+            {
+                string[] parts = Properties.Settings.Default.ConnectionString.Split(';');
+
+                string[] temp = parts[0].Split('=');
+                txtServerName.Text = temp[1];
+
+                temp = parts[1].Split('=');
+                txtDatabaseName.Text = temp[1];
+
+                string database = temp[1];
+
+                temp = parts[2].Split('=');
+                txtUserName.Text = temp[1];
+
+                temp = parts[3].Split('=');
+                txtPassword.Password = temp[1];
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
